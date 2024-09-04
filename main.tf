958593b00d44f2f2a183740ceff6edaf227d01f8
shrijandra <shrijandra@yahoo.com>
2024-09-04 14:30:29 -0500

    delete ec2

--- main.tf
+++ main.tf
@@ -18,10 +18,11 @@ terraform {
   }
 }
 
-resource "aws_instance" "test_server" {
-  ami = "ami-0490fddec0cbeb88b"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0ecf425ee928af0a7"
-  associate_public_ip_address = true
+#to delete resources
+# resource "aws_instance" "test_server" {
+#   ami = "ami-0490fddec0cbeb88b"
+#   instance_type = "t2.micro"
+#   subnet_id = "subnet-0ecf425ee928af0a7"
+#   associate_public_ip_address = true
   
-}
+# }
