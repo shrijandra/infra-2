5d52722999b329db3180b87d8538c0475c318d49
shrijandra <shrijandra@yahoo.com>
2024-09-29 02:58:36 -0500

    delete ec2

--- main.tf
+++ main.tf
@@ -12,17 +12,18 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "devopsinfra-2-tf-state"
+    bucket = "devops-tf-state-2"
     key = "platform.tfstate"
     region = "us-east-2"    
   }
 }
-
-#to delete resources
-# resource "aws_instance" "test_server" {
-#   ami = "ami-0490fddec0cbeb88b"
-#   instance_type = "t2.micro"
-#   subnet_id = "subnet-0ecf425ee928af0a7"
-#   associate_public_ip_address = true
+/*
+#to deletecreate resources
+resource "aws_instance" "test_server" {
+  ami = "ami-0490fddec0cbeb88b"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0ecf425ee928af0a7"
+  associate_public_ip_address = true
   
-# }
+} add new
+*/
